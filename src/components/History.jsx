93eaf75a51c65f259a606f47ed8f34936a3c4f0c
DavidTB00 <davidtb17@hotmail.no>
2026-03-02 13:59:43 +0100

    La til localstorage funksjoner og at søkene kommer opp på siden.

---
+++ src/components/History.jsx
@@ -0,0 +1,12 @@
+export default function Hitory({history, setSearch}){
+
+    const handleChange= (e)=>{
+        setSearch(e.target.value)
+    }
+    return (
+        <select onChange={handleChange}>
+            {history?.map((item, i) => <option key={i} value={item}>{item}</option>)}
+        </select>
+
+    )
+}
